b2415c63e069cebddeb53eb44ebe3dfeae2b84ef
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 11:40:35 +0700

    Add class definition for Ibu in AgregasiIbu.h

--- AgregasiIbu.h
+++ AgregasiIbu.h
@@ -2,3 +2,17 @@
 #define IBU_H
 #include <vector>
 
+class Ibu {
+public:
+    string nama;
+    vector<Anak*> anak;
+
+    ibu(string pnama) :nama(pnama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+     void tambahAnak(Anak*);
+     void cetakAnak();   
+};
