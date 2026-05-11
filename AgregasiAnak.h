2388bbbf71ced9b9ae74f6016a7ed651e0f90ed1
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 11:37:30 +0700

    Add class definition for Anak in AgregasiAnak.h

--- AgregasiAnak.h
+++ AgregasiAnak.h
@@ -1,3 +1,14 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+class Anak {
+public:
+    string nama;
+    anak(string pnama) :nama(pnama) {
+        cout << "Anak \"" << nama << "\" ada\n;
+    }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }    
+};
+#endif
