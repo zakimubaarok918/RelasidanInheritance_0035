77396c2fed68175084e60e31ee2f89e7266cd169
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 12:05:38 +0700

    Add Orang class with constructor, destructor, and jumlah method

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -2,3 +2,20 @@
 #include <string>
 using namespace std;
 
+class Orang {
+public:
+    string nama;
+
+    Orang(string pnama) :
+        nama(pnama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+    ~Orang() {
+        cout << "Orang dihapus\n" << endl;
+    }    
+
+     int jumlah(int a, int b) {
+        return a + b;
+   }
+
+}; 
