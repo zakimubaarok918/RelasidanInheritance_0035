2eb23fe369bc961b96fa25d1e0301691b910f97c
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 11:46:29 +0700

    Fix variable names and memory management in main function of FileMainAgregasi.cpp

--- FileMainAgregasi.cpp
+++ FileMainAgregasi.cpp
@@ -4,3 +4,28 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main() { // agregasi
+    Ibu* ibu1 = new Ibu("dini");
+    Anak* anak1 = new Ibu("novi");
+    anak* varAnak1 = new Anak("tono");
+    anak* varAnak2 = new Anak("rini");
+    anak* varAnak3 = new Anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+}    
