75d0d64873399bbec08288a0f45bc3f0d6bd89b5
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 12:13:10 +0700

    Add Manusia class with constructor and destructor for lifecycle management

--- manusia.h
+++ manusia.h
@@ -2,3 +2,17 @@
 #define MANUSIA_H
 using namespace std;
 
+class Manusia {
+public:
+    string name;
+    jantung varJantung;
+
+    Manusia(string pname) 
+         :name(pname) {
+        cout << name << " hidup\n";
+    }
+    ~Manusia() {
+        cout << name << " mati\n";
+    }
+};
+#endif    
