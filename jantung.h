d3865fd0e9bd06831720cf6c78e609b3cdc57104
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 12:09:09 +0700

    Add Jantung class with constructor and destructor for lifecycle management

---
+++ jantung.h
@@ -0,0 +1,15 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class Jantung {
+public:
+    jantung() {
+        cout << "Jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "Jantung dimatikan\n";
+    }
+};
+#endif    
