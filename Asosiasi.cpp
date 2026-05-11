f0165a083c930e349b856be2131f47cb830278ac
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 11:51:49 +0700

    Add pasien class definition with methods for managing doctors

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -2,3 +2,17 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_Dokter;
+    pasien(string pnama) :nama(pnama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }    
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
