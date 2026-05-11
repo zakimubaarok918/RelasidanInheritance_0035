bc429891e756312bf6e2c06d2683493eabee8db2
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 11:41:16 +0700

    Implement methods for adding and printing children in Ibu class

--- AgregasiIbu.h
+++ AgregasiIbu.h
@@ -16,3 +16,17 @@ public:
      void tambahAnak(Anak*);
      void cetakAnak();   
 };
+void ibu::tambahAnak(Anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+    cout << "Daftar anak dari Ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak) {
+        cout << "- " << a->nama << "\n";
+    }*/
+    for (int i = 0; i < daftar_anak.size(); ++i) {
+        cout <<  daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
+#endif
