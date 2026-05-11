5444b92c09211c57ffb215b551235454a99b3654
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-11 11:53:40 +0700

    Refactor dokter class and implement methods for managing pasien associations

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -16,3 +16,41 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_Pasien;
+
+    dokter(string pnama) :nama(pnama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }  
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_Dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_Dokter) { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diiterasi.
+        cout <<  daftar_Dokter[i]->nama << endl;
+    }
+    cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_Pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien() {
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_Pasien) {
+        cout <<  daftar_Pasien[i]->nama << endl;
+    }
+    cout << endl;
+}
